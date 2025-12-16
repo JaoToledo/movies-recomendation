d1fafeaef14623dc400a85e999befaceb9e5d8b6
João Toledo <joaovittortoledosb@gmail.com>
2025-12-16 12:11:43 -0300

    feat:(header): add search bar and align layout

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -3,11 +3,15 @@
  * @returns {JSX.Element} Header global da página
  */
 
+import { SearchBar } from "@/features/components/SearchBar/SearchBar";
+
 export function Header() {
   return (
     <div 
     data-testid="header"
-    className="flex bg-header-bg py-5 px-[280px] ">
+    className="flex justify-center bg-header-bg py-5 px-[280px]  "
+    >
+      <SearchBar />
     </div>
   )
 } 
