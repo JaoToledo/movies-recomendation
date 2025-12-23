e2255da8458eb2dc032136c01fe8fdcad444a6a4
João Toledo <joaovittortoledosb@gmail.com>
2025-12-23 16:43:51 -0300

    feat: inserting the header into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+"use client"
 import './globals.css';
-import { Header } from '@/shared/components/Header/Header';
+import { Header } from '@/shared/components/HeaderComposition';
 import {Poppins} from 'next/font/google'
+import { useState } from 'react'; 
+// import { useRouter } from 'next/router';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -9,16 +12,37 @@ const poppins = Poppins({
 })
 
 export default function RootLayout({
+
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+    }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+
+
+//  function handleRedirect(search:string){
+//    route.route('')}
+    
+   const [search, setSearch] = useState('');
+  
   return (
+
+
     <html lang="en">
       <body className={`${poppins.variable}`}>
-        <Header />
+        <Header.Root >
+
+          <div className="flex items-center w-full max-w-[556px]  ">
+          <Header.Input 
+          onSubmit={()=>{alert('Teste')}} 
+          onValueChange={(value)=>setSearch(value) } 
+          value={search}/>
+
+          <Header.SearchInputButton/>   
+          </div>
+          
+        </Header.Root> 
         {children}
       </body>
-    </html>
+    </html> 
   );
 }
