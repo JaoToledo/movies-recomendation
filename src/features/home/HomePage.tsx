648b1cb61a6e2e34c860e6a7af05fdab01e3a67e
João Toledo <joaovittortoledosb@gmail.com>
2025-12-25 10:06:25 -0300

    feat: add Container component

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,12 +1,20 @@
+import { Container } from "@/shared/components/Container/Container";
 import { Highlight } from "./components/Highlight/Highlight";
 
 export default function HomePage(){
   return(
     <div className="min-h-screen bg-linear-to-b from-linear-black to-linear-gray from-10%">
 
-      <div className="flex justify-center pt-24">
+      <div className="flex justify-center py-16">
         <Highlight />
       </div>
+
+      <div className="flex justify-center">
+        <Container className="p-[75px]">
+          <div></div>
+        </Container>
+      </div>
+
     </div>
   );
 }
