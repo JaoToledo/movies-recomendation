38715b6f298e59639dd6cd95fc7850e139bd6161
João Toledo <joaovittortoledosb@gmail.com>
2026-01-30 22:30:07 -0300

    feat: change AlterBadge component to CategoryMovieTitle component

---
+++ src/shared/components/CategoryMovieTitle/CategoryMovieTitle.tsx
@@ -0,0 +1,13 @@
+import { Typography } from "@/shared/components/Typography/Typography";
+
+interface CategoryMovieTitleProps {
+  children: string; 
+}
+
+export function CategoryMovieTitle({ children }: CategoryMovieTitleProps){
+  return (
+      <Typography variant="H1" className="font-bold text-white">
+        {children}
+      </Typography>
+  )
+}
